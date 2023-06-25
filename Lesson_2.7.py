bf012be59ba9222173c77119be9d1972a1c4ce00
Sasha Vasiliev <Alexandr@MacBook-Pro-Sasha.local>
2023-06-25 21:58:13 +0300

    Add Lesson_2.7 and txtfile

---
+++ Lesson_2.7.py
@@ -0,0 +1,17 @@
+# Реалізуйте програму, яка копіює вміст одного файлу в інший.
+
+def copy_file(txt_file_one, txt_file_two):
+    try:
+        with open(txt_file_one, "r") as file1:
+            with open(txt_file_two, "w") as file2:
+                file2.write(file1.read())
+        print("Файл успішно скопійовано.")
+    except FileNotFoundError:
+        print("Помилка! Файл не знайденно...")
+    except:
+        print("Помилка при копіюванні.")
+
+
+name_file1 = "Import_this.txt"
+name_file2 = "New_File.txt"
+copy_file(name_file1, name_file2)
